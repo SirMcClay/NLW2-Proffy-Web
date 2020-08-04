7119c794afd31d32dcc973d7c28344a88cc79b45
sirmcclay <mcclay.design@gmail.com>
2020-08-04 11:04:35 -0300

    Landing page responsive layout - mobile-first

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -7,6 +7,8 @@ import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
+import './styles.css'
+
 function Landing() {
   return (
     <div id="page-landing">
@@ -30,7 +32,7 @@ function Landing() {
 
           <a href="" className="give-classes">
             <img src={giveClassesIcon} alt="Estudar"/>
-            Estudar
+            Dar aulas
           </a>
         </div>
 
